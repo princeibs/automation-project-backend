f018bae19365e7371116eddff46e8849620fcbc6
Ibrahim <suleimanonnet@gmail.com>
2023-06-30 05:54:55 +0100

    Update Student model

--- src/models/Student.js
+++ src/models/Student.js
@@ -4,7 +4,7 @@ const StudentSchema = new mongoose.Schema({
     matricNo: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    otherNames: {type: String, required: true},
+    otherNames: {type: String},
     role: {type: String},
     password: {type: String, required: true},
     savedTopics: [{type: mongoose.Schema.Types.ObjectId, ref: "topics"}]
